68f0913453a9a1e43ecc6cfcdac15798fb4b8225
Ngan Lyle <bbeline@gmail.com>
2019-12-11 07:21:42 -0800

    updated insdose mod

--- healthmgr/medmgr/insdose.py
+++ healthmgr/medmgr/insdose.py
@@ -4,14 +4,29 @@
 # In[ ]:
 
 # File insdose.py
+import sys
+
+class NegNumError(Exception):
+    pass
+
+
 def bbdosing(kg):
     """Function to estimate the total daily insulin dose and the basal insulin dose based on weight in kg."""
-    total = round(kg * 0.5, 1)
-    basal = round(total * 0.4, 1)
-    bolus = round(total * 0.6, 1)
-    if kg <= 0:
-        raise ValueError('Enter a positive number.')
-    return('''Based on your weight in kg:
+    
+    try:
+        total = round(kg * 0.5, 1)
+        basal = round(total * 0.4, 1)
+        bolus = round(total * 0.6, 1)
+        if kg <= 0:
+            raise NegNumError()
+    except NegNumError:
+        return('Please enter a positive number greater than 0.')
+    except TypeError:
+        return('Please enter a number.')
+    except:
+        return(sys.exc_info(),"occurred.")
+    else:
+        return('''Based on your weight in kg:
     Your estimated total daily insulin dose is {} units.
     Your estimated daily dose for basal insulin is {} units.
     Your estimated daily dose for bolus insulin is {} units.
@@ -20,27 +35,55 @@ def bbdosing(kg):
 # 500 ÷ Total Daily Insulin Dose = 1 unit insulin covers so many grams of carbohydrate
 def carbratio(tdi):
     """Uses the "Rule of 500" to estimate the insulin to carbohydrate ratio using the total daily insulin dose."""
-    r = round(500/tdi,1)
-    if tdi <= 0:
-        raise ValueError('Enter a positive number.')
-    return('Based on your daily insulin requirement, your estimated insulin to carb ratio is {}, meaning that 1 unit of rapid acting insulin will cover {} g of carbohydrates.'.format(r, r))
+    try:
+        r = round(500/tdi,1)
+        if tdi < 0:
+            raise NegNumError()
+    except NegNumError:
+        return('Please enter a positive number greater than 0.')
+    except ZeroDivisionError:
+        return('Please enter a positive number greater than 0.')
+    except TypeError:
+        return('Please enter a number.')
+    except:
+        return(sys.exc_info(),"occurred.")
+    else:
+        return('Based on your daily insulin requirement, your estimated insulin to carb ratio is {}, meaning that 1 unit of rapid acting insulin will cover {} g of carbohydrates.'.format(r, r))
 
 # Correction Factor/Dose = 100 divided by the Total Daily Insulin Dose (TDI) = 1 unit of insulin will reduce the blood sugar so many mmol/l
 def cfact(tdi):
     """Uses the "100 Rule for Rapid Acting Insulin" to estimate the correction factor/dose of insulin."""
-    f = round(100/tdi, 1)
-    if tdi < 0:
-        raise ValueError('Enter a positive number.')
-#     if tdi == 0:
-#         raise ZeroDivisionError('Enter a positive number.')
-    return('Your correction factor/dose is {}, meaning that 1 unit of rapid acting insulin will decrease your blood sugar by {} mmol/l.'.format(f, f))
+    try:
+        f = round(100/tdi, 1)
+        if tdi < 0:
+            raise NegNumError()
+    except NegNumError:
+        return('Please enter a positive number greater than 0.')
+    except ZeroDivisionError:
+        return('Please enter a positive number greater than 0.')
+    except TypeError:
+        return('Please enter a number.')
+    except:
+        return(sys.exc_info(),"occurred.")
+    else:
+        return('Your correction factor/dose is {}, meaning that 1 unit of rapid acting insulin will decrease your blood sugar by {} mmol/l.'.format(f, f))
 
 # Carbohydrate coverage at a meal = Total grams of CHO in the meal ÷ grams of CHO disposed by 1 unit of insulin (gcarb)
 def carbcorr(carb, ratio):
     """Estimates the carbohydrate correction dose based on the number of carbohydrates to be consumed in grams and the known insulin to carbohydrate ratio."""
-    corr = carb/ratio
-    if ratio <= 0:
-        raise ValueError('Enter a positive number.')
-    if carb <= 0:
-        raise ValueError('Enter a positive number.')
-    return('Your carbohydrate correction dose is {}.'.format(corr))
+    try:
+        corr = carb/ratio
+        if ratio < 0 or carb < 0:
+            raise NegNumError()
+#         if carb < 0:
+#             raise NegNumError('Please enter a positive number greater than 0 for the number of carbohydrates.')
+    except NegNumError as ex:
+        return('Please enter only positive numbers greater than 0 as inputs.')
+    except ZeroDivisionError:
+        return('Please enter a positive number greater than 0 for the insulin to carboydrate ratio.')
+    except TypeError:
+        return('Please enter only numbers as inputs.')
+    except:
+        return(sys.exc_info(),"occurred.")
+    else:    
+        return('Your carbohydrate correction dose is {}.'.format(corr))
