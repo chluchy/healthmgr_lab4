e97aa6e61d744f7786f1dfc3ab8a1613fdf76629
Ngan Lyle <bbeline@gmail.com>
2019-12-11 10:06:31 -0800

    updated insdose mod and test files

--- healthmgr/medmgr/insdose.py
+++ healthmgr/medmgr/insdose.py
@@ -9,22 +9,23 @@ import sys
 class NegNumError(Exception):
     pass
 
-
 def bbdosing(kg):
     """Function to estimate the total daily insulin dose and the basal insulin dose based on weight in kg."""
     
     try:
+        if kg <= 0:
+            raise ValueError()
         total = round(kg * 0.5, 1)
         basal = round(total * 0.4, 1)
         bolus = round(total * 0.6, 1)
         if kg <= 0:
-            raise NegNumError()
-    except NegNumError:
+            raise ValueError()
+    except ValueError:
         return('Please enter a positive number greater than 0.')
     except TypeError:
         return('Please enter a number.')
-    except:
-        return(sys.exc_info(),"occurred.")
+#     except:
+#         return(sys.exc_info(),"occurred.")
     else:
         return('''Based on your weight in kg:
     Your estimated total daily insulin dose is {} units.
@@ -36,17 +37,15 @@ def bbdosing(kg):
 def carbratio(tdi):
     """Uses the "Rule of 500" to estimate the insulin to carbohydrate ratio using the total daily insulin dose."""
     try:
-        r = round(500/tdi,1)
         if tdi < 0:
-            raise NegNumError()
+            raise NegNumError()        
+        r = round(500/tdi,1)
     except NegNumError:
         return('Please enter a positive number greater than 0.')
     except ZeroDivisionError:
         return('Please enter a positive number greater than 0.')
     except TypeError:
         return('Please enter a number.')
-    except:
-        return(sys.exc_info(),"occurred.")
     else:
         return('Based on your daily insulin requirement, your estimated insulin to carb ratio is {}, meaning that 1 unit of rapid acting insulin will cover {} g of carbohydrates.'.format(r, r))
 
@@ -54,17 +53,15 @@ def carbratio(tdi):
 def cfact(tdi):
     """Uses the "100 Rule for Rapid Acting Insulin" to estimate the correction factor/dose of insulin."""
     try:
-        f = round(100/tdi, 1)
         if tdi < 0:
             raise NegNumError()
+        f = round(100/tdi, 1)
     except NegNumError:
         return('Please enter a positive number greater than 0.')
     except ZeroDivisionError:
         return('Please enter a positive number greater than 0.')
     except TypeError:
         return('Please enter a number.')
-    except:
-        return(sys.exc_info(),"occurred.")
     else:
         return('Your correction factor/dose is {}, meaning that 1 unit of rapid acting insulin will decrease your blood sugar by {} mmol/l.'.format(f, f))
 
@@ -72,18 +69,14 @@ def cfact(tdi):
 def carbcorr(carb, ratio):
     """Estimates the carbohydrate correction dose based on the number of carbohydrates to be consumed in grams and the known insulin to carbohydrate ratio."""
     try:
-        corr = carb/ratio
         if ratio < 0 or carb < 0:
             raise NegNumError()
-#         if carb < 0:
-#             raise NegNumError('Please enter a positive number greater than 0 for the number of carbohydrates.')
-    except NegNumError as ex:
+        corr = carb/ratio
+    except NegNumError:
         return('Please enter only positive numbers greater than 0 as inputs.')
     except ZeroDivisionError:
         return('Please enter a positive number greater than 0 for the insulin to carboydrate ratio.')
     except TypeError:
         return('Please enter only numbers as inputs.')
-    except:
-        return(sys.exc_info(),"occurred.")
     else:    
         return('Your carbohydrate correction dose is {}.'.format(corr))
--- testinsdose_mod.py
+++ testinsdose_mod.py
@@ -11,8 +11,7 @@ class TestInsDose(unittest.TestCase):
     def test_bbdosing(self):
         """ Test of bbdosing function to make sure the right output of
         total/basal/bolus insulin doses are obtained depending on the input
-        weight. Test also deals with errors including ValueErrors and
-        ZeroDivisionErrors
+        weight. 
         """
 
         #print('bbdosing_test')
@@ -31,24 +30,24 @@ class TestInsDose(unittest.TestCase):
     Your estimated daily dose for basal insulin is 16.0 units.
     Your estimated daily dose for bolus insulin is 24.0 units.
     Generally, bolus insulin is divided evenly between meals.''')
-        with self.assertRaises(ValueError):
-            bbdosing(0)
-            bbdosing(-5)
+        self.assertEqual(bbdosing(-5), 'Please enter a positive number greater than 0.')
+        self.assertEqual(bbdosing(0), 'Please enter a positive number greater than 0.')
+        self.assertEqual(bbdosing('sam'), 'Please enter a number.')
+
 
     def test_carbratio(self):
         """ Test of carbratio function which calculates the insulin to carb
-        ratio based on input total daily insulin dose. Test also deals with
-        errors including ValueErrors and ZeroDivisionErrors
+        ratio based on input total daily insulin dose. 
         """
+        
         #print('carbratio_test')
         self.assertEqual(carbratio(40), 'Based on your daily insulin requirement, your estimated insulin to carb ratio is 12.5, meaning that 1 unit of rapid acting insulin will cover 12.5 g of carbohydrates.')
         self.assertEqual(carbratio(48), 'Based on your daily insulin requirement, your estimated insulin to carb ratio is 10.4, meaning that 1 unit of rapid acting insulin will cover 10.4 g of carbohydrates.')
-        with self.assertRaises(ValueError):
-            carbratio(-8)
-            carbratio(-15)
-        with self.assertRaises(ZeroDivisionError):
-            carbratio(0)
+        self.assertEqual(carbratio(-8), 'Please enter a positive number greater than 0.')
+        self.assertEqual(carbratio(0), 'Please enter a positive number greater than 0.')
+        self.assertEqual(carbratio('sam'), 'Please enter a number.') 
 
+     
     def test_cfact(self):
         """ Test of cfact function which calculates the correction factor based
         on input total daily insulin dose. Test also deals with errors including 
@@ -57,12 +56,11 @@ class TestInsDose(unittest.TestCase):
         #print('cfact_test')
         self.assertEqual(cfact(40), 'Your correction factor/dose is 2.5, meaning that 1 unit of rapid acting insulin will decrease your blood sugar by 2.5 mmol/l.')
         self.assertEqual(cfact(48), 'Your correction factor/dose is 2.1, meaning that 1 unit of rapid acting insulin will decrease your blood sugar by 2.1 mmol/l.')
-        with self.assertRaises(ValueError):
-            cfact(-8)
-            cfact(-15)
-        with self.assertRaises(ZeroDivisionError):
-            cfact(0)
+        self.assertEqual(cfact(-8), 'Please enter a positive number greater than 0.')
+        self.assertEqual(cfact(0), 'Please enter a positive number greater than 0.')
+        self.assertEqual(cfact('sam'), 'Please enter a number.') 
 
+            
     def test_carbcorr(self):
         """ Test of carbcorr function which calculates the carb correction dose
         based 2 inputs: carbratio and the total daily insulin dose. Inputs can
@@ -72,11 +70,17 @@ class TestInsDose(unittest.TestCase):
         #print('carbcorr_test')
         self.assertEqual(carbcorr(60, 10), 'Your carbohydrate correction dose is 6.0.')
         self.assertEqual(carbcorr(60, 12.5), 'Your carbohydrate correction dose is 4.8.')
-        with self.assertRaises(ValueError):
-            carbcorr(60, -8)
-            carbcorr(0, 10)
-            carbcorr(-60, 10)
-        with self.assertRaises(ZeroDivisionError):
-            carbcorr(60, 0)
-
+        self.assertEqual(carbcorr(-8, 10), 'Please enter only positive numbers greater than 0 as inputs.')
+        self.assertEqual(carbcorr(60, -5), 'Please enter only positive numbers greater than 0 as inputs.')
+        self.assertEqual(carbcorr(-60, -5), 'Please enter only positive numbers greater than 0 as inputs.')
+        self.assertEqual(carbcorr(60, 0), 'Please enter a positive number greater than 0 for the insulin to carboydrate ratio.')
+        self.assertEqual(carbcorr('sam', 10), 'Please enter only numbers as inputs.')
+        self.assertEqual(carbcorr(60, 'sam'), 'Please enter only numbers as inputs.')
+        self.assertEqual(carbcorr('bob', 'sam'), 'Please enter only numbers as inputs.')
+#         with self.assertRaises(NegNumError):
+#             carbcorr(60, -8)
+#             carbcorr(0, 10)
+#             carbcorr(-60, 10)
+#         with self.assertRaises(ZeroDivisionError):
+#             carbcorr(60, 0)      
 unittest.main(argv=[''], verbosity=2, exit=False)
